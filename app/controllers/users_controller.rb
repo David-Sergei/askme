730f958d0997d5a859429f6c2998587e25d392ef
David-Sergei <david.siarhei@gmail.com>
2023-05-05 22:36:20 +0300

    rails validation+rails callbacks+delete user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,8 @@ class UsersController < ApplicationController
   end
 
   def create
-    user_params = params.require(:user).permit(:user, :nickname, :email, :password)
-
     @user = User.new(user_params)
+    
     if @user.save
       redirect_to root_path, notice: 'Вы успешно заригистрировались!'
     else
@@ -14,5 +13,36 @@ class UsersController < ApplicationController
 
       render :new
     end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to root_path, notice: 'Данные пользователя обновлены'
+    else
+      flash.now[:alert] = 'При попытке сохранить пользователя возникли ошибки'
+
+      render :edit
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    
+    session.delete(:user_id)
+
+    redirect_to root_path notice: 'Пользователь удален'
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:user, :nickname, :email, :password, :password_confirmation)
   end
 end
